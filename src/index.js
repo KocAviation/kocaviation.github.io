b1de1646ab6eb16f1328eb4384870429acd25e08
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-02-24 01:57:22 +0300

    Re-implementation
    Co-authored-by: Yigit Kerem Oktay <yigitkerem@theskyfallen.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-let serial = "1232123";
+var serial = "";
+//var objectData = {};
+
+async function setSerial() {
+  serial = $("#serial-input").val();
+  $("#initial").addClass("hidden");
+  $("#position-selector").removeClass("hidden");
+}
 
 $(window).on("load", function () {
-  new QRCode("test", {
+  for (let i = 1; i <= 14; i++) {
+    $("#position-selector")
+      .children()
+      .last()
+      .attr("onclick", `createSticker(${i})`);
+    if (i != 14) {
+      $("#position-selector").append($(".sticker-picker").last().clone());
+    }
+  }
+});
+
+function createSticker(posIndex) {
+  $("#position-selector").addClass("hidden");
+  $("#page-created").removeClass("hidden");
+  for (let i = 1; i <= 14; i++) {
+    if (i == posIndex) {
+      $("#paper").append(
+        $("#sticker-template").clone().removeAttr("id").removeClass("hidden")
+      );
+    } else {
+      $("#paper").append(
+        $("#sticker-buffer").clone().removeAttr("id").removeClass("hidden")
+      );
+    }
+  }
+  $("#sticker-template").remove();
+  $("#serial").text(serial);
+  new QRCode(document.getElementById("qr"), {
     text: `https://kocaviation.github.io/item.html?serial=${serial}`,
-    width: 256,
-    height: 256,
+    width: 100,
+    height: 100,
     colorDark: "#000000",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.L,
   });
-});
+  print();
+}
